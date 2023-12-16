a0442946b14af802774a4063760612bf4ec12f0c
Rodrigo Buges <99702723+rod-buges@users.noreply.github.com>
2023-12-16 18:45:35 -0300

    Add files via upload

---
+++ loading.js
@@ -0,0 +1,4 @@
+setTimeout(function() {
+    window.location.href = 'warning.html';
+  }, 10000); // 10 segundos de carregamento
+  
---
+++ script.js
@@ -0,0 +1,7 @@
+function cadastrar() {
+    // Aqui você pode adicionar a lógica para processar os dados do formulário
+    // e redirecionar para a próxima página.
+    alert('Os dados estão corretos? Deseja Prosseguir?');
+    window.location.href = 'loading.html';
+  }
+  
